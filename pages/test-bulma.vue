5d4dc1a452713534fa88fe22be9c38185b3f617f
Diego Ávila <diego.avila@usach.cl>
2020-09-29 18:15:38 -0300

    first commit

---
+++ pages/test-bulma.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="container">
+        
+        <section class="hero">
+            <div class="hero-body">
+    <div class="container">
+      <h1 class="title">
+        Hero title
+      </h1>
+      <h2 class="subtitle">
+        Hero subtitle
+      </h2>
+    </div>
+  </div>
+        </section>
+  
+  <div class="columns">
+    <div class="column">
+      First column
+    </div>
+    <div class="column">
+      Second column
+    </div>
+  </div>
+  <div class="columns">
+    <div class="column">
+      First column
+    </div>
+    <div class="column">
+      Second column
+    </div>
+    <div class="column">
+      Third column
+    </div>
+  </div>
+  <div class="columns">
+    <div class="column">
+      First column
+    </div>
+    <div class="column">
+      Second column
+    </div>
+    <div class="column">
+      Third column
+    </div>
+    <div class="column">
+      Fourth column
+    </div>
+  </div>
+  <div class="columns">
+    <div class="column">
+      <div class="field">
+        <label class="label">Subject</label>
+        <div class="control">
+            <div class="select">
+            <select>
+                <option>Select dropdown</option>
+                <option>With options</option>
+            </select>
+            </div>
+        </div>
+        </div>
+
+        <div class="field">
+        <label class="label">Message</label>
+        <div class="control">
+            <textarea class="textarea" placeholder="Textarea"></textarea>
+        </div>
+        </div>
+        <div class="field">
+            <div class="buttons">
+                <a class="button is-primary">Primary</a>
+                <a class="button is-link">Link</a>
+            </div>
+        </div>
+    </div>
+    <div class="column is-two-thirds">
+      Second column
+    </div>
+  </div>
+</div>
+</template>
